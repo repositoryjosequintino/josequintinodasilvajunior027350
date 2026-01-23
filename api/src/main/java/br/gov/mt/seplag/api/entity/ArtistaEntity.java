ed8747c3799655fbe3c91c586651254134f29ebe
José Quintino <josequintino@hotmail.com.br>
2026-01-23 19:05:49 -0300

    feat: Implementar mapeamento da entidades persistentes - artista, álbum e álbum-artista (#6)

---
+++ api/src/main/java/br/gov/mt/seplag/api/entity/ArtistaEntity.java
@@ -0,0 +1,105 @@
+package br.gov.mt.seplag.api.entity;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_ARTISTA")
+public class ArtistaEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CODE", unique = true, nullable = false)
+    private Long code;
+
+    @Column(name = "CODE_PUBLIC", unique = true, nullable = false)
+    private UUID codePublic = UUID.randomUUID();
+
+    @Column(name = "NOME", unique = true, nullable = false)
+    private String nome;
+    
+    @Column(name = "CREATED_AT", updatable = false, nullable = false)
+    private Instant createdAt = Instant.now();
+
+    @Column(name = "UPDATED_AT")
+    private Instant updatedAt;
+
+    @Column(name = "DELETED_AT")
+    private Instant deletedAt;
+
+    @Column(name = "IS_ACTIVE", nullable = false)
+    private Boolean isActive = true;
+    
+    public ArtistaEntity() {}
+
+	public Long getCode() {
+		return code;
+	}
+
+	public void setCode(Long code) {
+		this.code = code;
+	}
+
+	public UUID getCodePublic() {
+		return codePublic;
+	}
+
+	public void setCodePublic(UUID codePublic) {
+		this.codePublic = codePublic;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Instant createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Instant updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public Instant getDeletedAt() {
+		return deletedAt;
+	}
+
+	public void setDeletedAt(Instant deletedAt) {
+		this.deletedAt = deletedAt;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
