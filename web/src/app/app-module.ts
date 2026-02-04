f71e77fead77bcf2b55808c99bdb86cca947781f
José Quintinno <josequintino@hotmail.com.br>
2026-02-04 17:16:33 -0300

    feat: Criar componente de header

--- web/src/app/app-module.ts
+++ web/src/app/app-module.ts
@@ -1,53 +1,25 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing-module';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { App } from './app';
-import { ArtistaComponent } from './components/artista-component/artista-component';
-import { ArtistaCadastrarComponent } from './components/artista/artista-cadastrar-component/artista-cadastrar-component';
-import { ArtistaEditarComponent } from './components/artista/artista-editar-component/artista-editar-component';
-import { ArtistaDetalharComponent } from './components/artista/artista-detalhar-component/artista-detalhar-component';
-import { Album } from './components/album/album';
-import { AlbumCadastrarComponent } from './components/album/album-cadastrar-component/album-cadastrar-component';
-import { AlbumEditarComponent } from './components/album/album-editar-component/album-editar-component';
-import { AlbumDetalharComponent } from './components/album/album-detalhar-component/album-detalhar-component';
-import { AlbumDetalharPage } from './pages/album/album-detalhar-page/album-detalhar-page';
-import { AlbumCadastrarPage } from './pages/album/album-cadastrar-page/album-cadastrar-page';
-import { AlbumEditarPage } from './pages/album/album-editar-page/album-editar-page';
-import { Artista } from './pages/artista/artista';
-import { ArtistaCadastrarPage } from './pages/artista/artista-cadastrar-page/artista-cadastrar-page';
-import { ArtistaDetalharPage } from './pages/artista/artista-detalhar-page/artista-detalhar-page';
-import { ArtistaEditarPage } from './pages/artista/artista-editar-page/artista-editar-page';
-import { HeaderComponent } from './components/header-component/header-component';
+import { AppRoutingModule } from './app-routing-module';
 import { FooterComponent } from './components/footer-component/footer-component';
+import { HeaderComponent } from './components/header-component/header-component';
+import { MaterialModule } from './material-module';
 
 @NgModule({
   declarations: [
     App,
-    ArtistaComponent,
-    ArtistaCadastrarComponent,
-    ArtistaEditarComponent,
-    ArtistaDetalharComponent,
-    Album,
-    AlbumCadastrarComponent,
-    AlbumEditarComponent,
-    AlbumDetalharComponent,
-    AlbumDetalharPage,
-    AlbumCadastrarPage,
-    AlbumEditarPage,
-    Artista,
-    ArtistaCadastrarPage,
-    ArtistaDetalharPage,
-    ArtistaEditarPage,
     HeaderComponent,
     FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideClientHydration(withEventReplay()),
   ],
   bootstrap: [App]
 })
---
+++ web/src/app/material-module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+const materialModules = [
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSnackBarModule,
+  MatTableModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatBadgeModule
+];
+
+@NgModule({
+  imports: [...materialModules],
+  exports: [...materialModules],
+})
+export class MaterialModule {}
