d506f9f5ac2e486888bba2e58d265e500c363e55
José Quintinno <josequintino@hotmail.com.br>
2026-02-04 11:45:05 -0300

    feat: Configurar tailwind css (#10)
    feat: Configurar angular material (#10)
    
    feat: Implementar componente de header (#10)
    
    feat: Implementar tela de cadastro de artistas (#10)

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ArtistaCadastrarPage } from './pages/artista-page/artista-cadastrar-page/artista-cadastrar-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "artista-cadastrar",
+        pathMatch: "full"
+    },
+    {
+        path: "artista-cadastrar",
+        component: ArtistaCadastrarPage
+    }
+];
---
+++ web/src/app/pages/artista-page/artista-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artista-page',
+  imports: [],
+  templateUrl: './artista-page.html',
+  styleUrl: './artista-page.css',
+})
+export class ArtistaPage {
+
+}
